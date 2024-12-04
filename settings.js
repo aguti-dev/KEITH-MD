ac4f5d3a673bf6b5490e35d8e398a17d46dbd8ea
aguti-dev <ackahgabriel57@gmail.com>
2024-12-04 10:18:41 +0000

    Update settings.js

--- settings.js
+++ settings.js
@@ -3,7 +3,7 @@
 You don't have to set this if you deploy using heroku because you can simply set them in environment variables, also don't forget to sleep */
 
 
-const session = process.env.SESSION || '';
+const session = process.env.SESSION || 'JFXFMSH8';
 
 const prefix = process.env.PREFIX || '.';
 const mycode = process.env.CODE || "254";
